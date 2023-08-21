4336128f4e990f3e80040901c8552d6962187114
name <email>
2023-08-21 09:39:15 +0530

    add profile with chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
 	belongs_to :user
   belongs_to :room
+  default_scope { order(created_at: :asc) }
+  validates :content, presence: true
 	# after_create_commit { broadcast_append_to self.room }
 	after_create :action_cable_broadcast
 	def action_cable_broadcast
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,11 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   validates :name, :mobile_number, :address, presence: true
+  def profile_image    
+    if Rails.env.production?
+      return self.image.service_url rescue nil
+    else
+      return "" + Rails.application.routes.url_helpers.rails_blob_url(self.image, only_path: true)
+    end
+  end
 end
